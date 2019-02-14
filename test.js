8c5a5fa74a29822fa0a861455176d46ef47c4851
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-14 22:48:29 +0000

    Automatically backed up by Learn

---
+++ test.js
@@ -0,0 +1,32 @@
+function find(array, criteriaFn){
+  for (let i = 0; i < array.length: i++){
+    if (criteriaFn(array[i])){
+      return array[i]
+    }
+  }
+}
+
+
+
+
+
+
+
+function find(array, criteriaFn){
+  let current = array
+  let next = []
+  
+  while (current){
+    if (criteriaFn(current)){
+      return current
+    }
+    
+    if (Array.isArray(current)){
+      for (let i = 0; i < current.length; i++){
+        next.push(current(i))
+      }
+      current = next.shift()
+    }
+  }
+  return null
+}
